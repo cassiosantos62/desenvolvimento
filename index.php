3b0191a8399a8ca3777bbd9174baf24d0f04c5e0
cassiosantos62 <cassiodossantoscosta62@gmail.com>
2015-07-27 22:26:54 -0300

    Subindo INDEX

---
+++ index.php
@@ -0,0 +1,26 @@
+ <?php
+include  'head.php';
+?>
+<html>
+<body>
+  <div class="container-fluid">
+      <div class="row header-redes">
+        <i class="fa fa-whatsapp"></i>
+        <i class="fa fa-youtube-play"></i>
+        <i class="fa fa-linkedin-square"></i>
+        <i class="fa fa-twitter-square"></i>
+        <i class="fa fa-instagram"></i>
+        <i class="fa fa-facebook-official"></i>
+      </div>
+      <div class="logo-">
+
+      </div>
+
+
+    </div>
+  </div>
+
+
+
+  </body>
+  </html>
